4c5623b6fb009c08696193d915b76a8a0bdca200
Nick Patel <nickpatel17042006@gmail.com>
2025-07-12 11:35:23 +0530

    Your message describing what you changed

--- frontend/front_skill_swap_project/src/App.jsx
+++ frontend/front_skill_swap_project/src/App.jsx
@@ -1,16 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import UserCard from "./components/UserCard";
+import Pagination from "./components/Pagination";
 
-function App() {
-  const [count, setCount] = useState(0)
+const users = [
+  {
+    name: "Marc Demo",
+    offeredSkills: ["JavaScript", "Python"],
+    wantedSkills: ["Photoshop", "Graphic Designer"],
+    rating: 3.9,
+  },
+  {
+    name: "Michell",
+    offeredSkills: ["JavaScript", "Python"],
+    wantedSkills: ["Photoshop", "Graphic Designer"],
+    rating: 2.5,
+  },
+  {
+    name: "Joe Wills",
+    offeredSkills: ["JavaScript", "Python"],
+    wantedSkills: ["Photoshop", "Graphic Designer"],
+    rating: 4.0,
+  },
+];
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState(1);
 
   return (
-    <>
-    Hello Everyone !!
-    </>
-  )
-}
+    <div className="min-h-screen bg-black text-white">
+      <Navbar />
+      <div className="p-6 max-w-4xl mx-auto">
+        {users.map((user, index) => (
+          <UserCard key={index} {...user} />
+        ))}
+        <Pagination
+          totalPages={7}
+          currentPage={currentPage}
+          onPageChange={setCurrentPage}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
